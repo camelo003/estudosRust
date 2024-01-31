314cae7029053b0e24c2a14f614daf99960f0391
camelo003 <holysmoke003@gmail.com>
2024-01-31 04:35:02 -0300

    Algum avanço c/ mapa e vetor "plane":
    - Mapa carregavel via string literal de 0's e 1's;
    - Tamanho de "plane" calculado a partir da const DIR_LENGTH
      (FOV de 50 deg.);
    - "plane" é renderizado junto ao player e rotaciona de acordo

--- 05_raycaster/raycaster/src/main.rs
+++ 05_raycaster/raycaster/src/main.rs
@@ -1,18 +1,77 @@
 use bracket_lib::prelude::*;
-use bracket_geometry::prelude::*;
+// use bracket_geometry::prelude::*;
 
-const PLAYER_RADIUS: i32 = 2;
-const DIR_LENGTH: f32 = 4.0;
-const ROT_STEP: f32 = 0.2;
+const SCREEN_W: i32 = 80;
+const SCREEN_H: i32 = 50;
+
+const PLAYER_RADIUS: i32 = 1;
+const DIR_LENGTH: f32 = 15.0;
+const ROT_STEP: f32 = 0.1;
+
+const TILE_SIZE: i32 = 5;
+
+const TILES_W: i32 = SCREEN_W / TILE_SIZE;
+const TILES_H: i32 = SCREEN_H / TILE_SIZE;
 
 enum GameMode {
 	ThreeD,
 	TwoD,
 }
 
+struct Map {
+	tiles: Vec<bool>,
+}
+
+impl Map {
+	fn new() -> Self {
+		let mut temp_tiles: Vec<bool> = vec![];
+		for i in concat!("1111111111111111",
+						 "1000000000010001",
+						 "1000011000010001",
+						 "1000001000010001",
+						 "1000000000000001",
+						 "1000100000000001",
+						 "1000100001111001",
+						 "1000100001000001",
+						 "1000100000000001",
+						 "1111111111111111").chars() {
+			if i == '1' {
+				temp_tiles.push(true);
+			}else{
+				temp_tiles.push(false);
+			}
+		}
+		Self {
+			tiles: temp_tiles,
+		}
+	}
+	fn render (&self, ctx: &mut BTerm) {
+		let mut counter = 0;
+		for i in &self.tiles {
+			let _x1 = (counter * TILE_SIZE) % SCREEN_W;
+			let _y1 = counter / TILES_W * TILE_SIZE;
+			let _x2 = _x1 + TILE_SIZE;
+			let _y2 = _y1 + TILE_SIZE;
+			if *i {
+				let r: Rect = Rect {x1: _x1, y1: _y1, x2: _x2, y2: _y2};
+				r.for_each(|p: Point| ctx.set(p.x, p.y, GREY, BLACK, '+'));
+			}else{
+				let l1: Bresenham = Bresenham::new(Point {x: _x1, y: _y1},
+												   Point {x: _x2, y: _y1});
+				let l2: Bresenham = Bresenham::new(Point {x: _x1, y: _y1},
+												   Point {x: _x1, y: _y2});
+				l1.for_each(|p: Point| ctx.set(p.x, p.y, GREY, BLACK, '+'));
+				l2.for_each(|p: Point| ctx.set(p.x, p.y, GREY, BLACK, '+'));
+			}
+			counter = counter + 1;
+		}
+	}
+}
+
 struct State {
 	mode: GameMode,
 	player: Player,
+	map: Map,
 }
 
 impl State {
@@ -20,6 +79,7 @@ impl State {
 		Self {
 			mode: GameMode::TwoD,
 			player: Player::new(),
+			map: Map::new(),
 		}
 	}
 	fn three_d(&mut self, ctx: &mut BTerm) {
@@ -29,6 +89,7 @@ impl State {
 	fn two_d(&mut self, ctx: &mut BTerm) {
 		ctx.cls();
 		ctx.print(1, 1, "Hellow 2D World!");
+		self.map.render(ctx);
 		self.player.update(ctx);
 		self.player.render(ctx);
 	}
@@ -42,10 +103,11 @@ struct Player {
 
 impl Player {
 	fn new() -> Self {
+		let p = f32::tan(0.436332) * DIR_LENGTH;
 		Self {
 			camera: [10.0, 10.0],
 			direction: [DIR_LENGTH, 0.0],
-			plane: [0.0, 1.0],
+			plane: [0.0, p],
 		}
 	}
 	fn normalize(v: [f32; 2]) -> [f32; 2] {
@@ -84,13 +146,21 @@ impl Player {
 					self.direction = Self::rot(
 						self.direction,
 						-ROT_STEP
-					)
+					);
+					self.plane = Self::rot(
+						self.plane,
+						-ROT_STEP
+					);
 				},
 				VirtualKeyCode::D => {
 					self.direction = Self::rot(
 						self.direction,
 						ROT_STEP
-					)
+					);
+					self.plane = Self::rot(
+						self.plane,
+						ROT_STEP
+					);
 				},
 				_ => {},
 			}
@@ -105,13 +175,23 @@ impl Player {
 			self.direction[0].round() as i32,
 			self.direction[1].round() as i32
 		);
-		ctx.set(pos.x, pos.y, WHITE, BLACK, 'P');
+		let pln = Point::new(
+			(self.plane[0] / 2.0).round() as i32,
+			(self.plane[1] / 2.0).round() as i32
+		);
+		for point in Bresenham::new(pos, pos + dir + pln) {
+			ctx.set(point.x, point.y, WHITE, BLACK, '*');
+		}
+		for point in Bresenham::new(pos, pos + dir - pln) {
+			ctx.set(point.x, point.y, WHITE, BLACK, '*');
+		}
+		for point in Bresenham::new(pos + dir + pln, pos + dir - pln) {
+			ctx.set(point.x, point.y, WHITE, BLACK, '*');
+		}
 		for point in BresenhamCircle::new(pos, PLAYER_RADIUS) {
-			ctx.set(point.x, point.y, WHITE, BLACK, '*');
+			ctx.set(point.x, point.y, GREEN, BLACK, '*');
 		}
-		for point in Bresenham::new(pos, pos + dir) {
-			ctx.set(point.x, point.y, WHITE, BLACK, '*');
-		}
+		ctx.set(pos.x, pos.y, WHITE, BLACK, 'P');
 	}
 }
 
